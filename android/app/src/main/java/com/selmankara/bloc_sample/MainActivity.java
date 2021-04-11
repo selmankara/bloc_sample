3eb0405c33d147c0874f26caf672a25006dfc7ff
selmankara <selman@selmankara.com>
2021-04-11 15:04:09 +0300

    v1

---
+++ android/app/src/main/java/com/selmankara/bloc_sample/MainActivity.java
@@ -0,0 +1,6 @@
+package com.selmankara.bloc_sample;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/models/product.dart
@@ -0,0 +1,20 @@
+class Product {
+  int id;
+  String name;
+  double price;
+
+  // Product(int urunId, String urunName, double urunPrice){
+  //   id=urunId;
+  //   name=urunName;
+  //   price=urunPrice;
+  // }
+
+  // Product(int id, String name, double price) {
+  //   this.id = id;
+  //   this.name = name;
+  //   this.price = price;
+  // }
+
+  //Product({this.id, this.name, this.price});
+  Product(this.id, this.name, this.price);
+}
